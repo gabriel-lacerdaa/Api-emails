1cf77c979b324208fc1b4db31e5a908bc9cd8c92
Gabriel Lacerda <gabriel.lacerda1@outlook.com>
2023-06-20 13:01:33 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,59 +1,58 @@
-const express = require('express')
-const bodyParser = require('body-parser');
+const express = require('express');
 const mysql = require('mysql');
 require('dotenv').config();
-const cors = require('cors')
+const cors = require('cors');
 
-const MYSQL_USER = process.env.MYSQL_USER
-const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
-const MYSQL_DATABASE = process.env.MYSQL_DATABASE
-const MYSQL_HOST = process.env.MYSQL_HOST
-
+const MYSQL_USER = process.env.MYSQL_USER;
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE;
+const MYSQL_HOST = process.env.MYSQL_HOST;
 
 const app = express();
 const port = 5000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json()); // Middleware para parsing do corpo das requisições como JSON
 
 const db = mysql.createConnection({
-    host: MYSQL_HOST,
-    user: MYSQL_USER,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DATABASE
-})
+  host: MYSQL_HOST,
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
+});
 
 db.connect((err) => {
-    if (err){
-        throw err
-    }
-})
+  if (err) {
+    throw err;
+  }
+  console.log('Conexão com o banco de dados estabelecida!');
+});
 
-app.get('/', async (req, res) => {
-    const sql = 'Select * from email';
-    await db.query(sql, (err, result) => {
-        if (err) {
-            throw err;
-          }
-          res.send(result)
-    })
-})
+app.get('/', (req, res) => {
+  const sql = 'SELECT * FROM email';
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.send(result);
+  });
+});
 
 app.post('/email', (req, res) => {
-    const { nome, email, telefone, mensagem } = req.body;
-    console.log(nome, email, telefone, mensagem)
-    const sql = 'INSERT INTO email (nome, email, telefone, mensagem) VALUES (?, ?, ?, ?)';
-    const values = [nome, email, telefone, mensagem];
-    db.query(sql, values, (err, result) => {
-        if (err) {
-          throw err;
-        }
-        console.log('Email salvo no banco de dados!');
-        res.status(200).send("email salvo com sucesso!!");
-      });
-})
-
+  const { nome, email, telefone, mensagem } = req.body;
+  console.log(nome, email, telefone, mensagem);
+  const sql = 'INSERT INTO email (nome, email, telefone, mensagem) VALUES (?, ?, ?, ?)';
+  const values = [nome, email, telefone, mensagem];
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Email salvo no banco de dados!');
+    res.status(200).send('Email salvo com sucesso!!');
+  });
+});
 
 // Iniciar o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-  });
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
